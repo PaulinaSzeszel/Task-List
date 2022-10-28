e050fa4549af886e77bfc3ff8edef98168297a49
Paulina Szeszel Web Developer <86908540+PaulinaSzeszel@users.noreply.github.com>
2022-10-28 15:49:09 +0200

    added filter tasks

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ function loadEventListener() {
   form.addEventListener('submit', addTask);
   taskList.addEventListener('click', removeTask);
   clearBtn.addEventListener('click', clearTasks);
+  filter.addEventListener('keyup', filterTasks);
 }
 
 function addTask(e) {
@@ -53,4 +54,18 @@ function clearTasks() {
   while (taskList.firstChild) {
     taskList.removeChild(taskList.firstChild);
   }
+}
+
+//Filter Tasks
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
+
+  document.querySelectorAll('.collection-item').forEach(function (task) {
+    const item = task.firstChild.textContent;
+    if (item.toLowerCase().indexOf(text) != -1) {
+      task.style.display = 'block';
+    } else {
+      task.style.display = 'none';
+    }
+  });
 }
